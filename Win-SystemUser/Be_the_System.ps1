9be7603395e55be0fefd079c726646f5fa75410e
Simon F <github@19r.de>
2017-09-27 12:39:23 +0200

    v0.1
    Erster Entwurf

---
+++ Win-SystemUser/Be_the_System.ps1
@@ -0,0 +1,13 @@
+﻿cd C:\
+mkdir temp
+cd C:\temp\
+wget http://download.sysinternals.com/files/PSTools.zip -OutFile PSTools.zip
+
+Add-Type -AssemblyName System.IO.Compression.FileSystem
+[System.IO.Compression.ZipFile]::ExtractToDirectory("C:\temp\PSTools.zip", "C:\temp\")
+
+Remove-Item -Path C:\temp\*.exe -Exclude "PsExec.exe", "PsExec64.exe"
+Remove-Item -Path C:\temp\*.chm
+Remove-Item -Path C:\temp\psversion.txt
+
+Start-Process .\PsExec.exe -ArgumentList "-i -s -d cmd.exe /accepteula"
